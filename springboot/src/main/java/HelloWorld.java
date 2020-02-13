c2b8bd9f7ba3f0774d11bd42c01cdc6437f2e419
Jesray Garciano <jesray.garciano@gmail.com>
2020-02-13 15:45:06 +0800

    Fixed port issues on tomcat

--- springboot/src/main/java/HelloWorld.java
+++ springboot/src/main/java/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.javainterviewpoint;
+//package com.javainterviewpoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
